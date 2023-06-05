d2d0ab0f4283d24fafce51ba76b4686d4cafb020
Jasmine <jasminehweying@gmail.com>
2023-06-05 22:08:45 +0800

    Initial Commit

---
+++ Developers/DevelopersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopersApp.swift
+//  Developers
+//
+//  Created by Jasmine Lai Hweeying on 05/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct DevelopersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
